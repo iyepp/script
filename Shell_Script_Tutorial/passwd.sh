6f9b31c9b36b950b4ebc4fc2a704badd113bb1ad
iyepp <iyepp@naver.com>
2019-12-11 19:04:21 +0900

    reset init

--- Shell_Script_Tutorial/passwd.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-read -p 'username: ' user_name
-read -sp 'password: ' password
-echo "username : $user_name"
-echo "password : $password"
--- Shell_Script_Tutorial/username.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#echo "Enter names : "
-read "Enter names : " -ap names
-echo "Names : ${names[0]} ${names[1]}"
